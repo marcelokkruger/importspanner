bbf69b239efb043f11e62e320e11aed018e56077
Marcelo Kruger <marcelo.kruger@neoway.com.br>
2024-12-11 14:08:09 -0300

    Adiciona novos tipos de dados e realiza melhoria no processo de checagem das variaveis

--- README.md
+++ README.md
@@ -26,16 +26,17 @@ Below you can see the command line used to import data
 
 Parameters
 
-Arg | Explain
---- | ---
-project_id | Your Google Project ID
-instance_id | Your Cloud Spanner instance ID
-database_id | Your Cloud Spanner database ID
-table_id | Your table name
-batch_size | Number of rows to insert in a batch
-data_file | Path of csv input data file
-head_file | If the header exists in the CSV, set the option to true to disregard it
-schema_file | Path of schema file describing the input data file
+Arg | Explain | Details
+--- | --- | ---
+project_id | Your Google Project ID | 
+instance_id | Your Cloud Spanner instance ID | 
+database_id | Your Cloud Spanner database ID | 
+table_id | Your table name | 
+batch_size | Number of rows to insert in a batch | 
+data_file | Path of csv input data file | 
+head_file | If the header exists in the CSV, set the option to true to disregard it | 
+schema_file | Path of schema file describing the input data file | 
+format_date | Sets the date format for conversion | Default is: %Y-%m-%d. For more formats check https://www.w3schools.com/python/python_datetime.asp
 
 # Example
 
@@ -43,5 +44,28 @@ In the example folder, there are 3 files that will help you with your first impo
 
 * person.ddl: This file contains the code for creating the table. This code must be run first in Spanner. The import does not create the table; it must be previously created.
 * person.schema: This is the schema of the data that will be imported. The application will read this schema file, and for each column listed in it, it will convert the data read from person.csv. The schema file is always organized as follows: the first column is the name of the field in the data file, and the second column is the data type.
+
+Schema Format
+
+    field,type
+
+Supported data types are:
+
+    integer
+    float
+    bytes
+    date
+    string
+    boolean
+
+Schema Example
+
+    document,string
+    name,string
+    dt_nascimento,date
+    nu_idade_obito,integer
+    id,integer
+
+
 * person.csv: Data files are always separated by commas.
 
--- import.py
+++ import.py
@@ -1,36 +1,72 @@
 import argparse
 import csv
+from decimal import Decimal
 import numbers
 import base64
 import os
 import warnings
+from datetime import datetime
 
 from google.cloud import spanner
 
 def castFloat(value):
   try:
-    return float(value)
-  except ValueError:
-    return 0.0
+    dec = Decimal(value)
+    return dec
+  except:
+    return None
     
 def castInt(value):
 	try:
 		return int(value)
 	except ValueError:
-		return 0
+		return None
+   
+def castBytes(value):
+	try:
+		return base64.b64encode(value)
+	except ValueError:
+		return None
+
+def castDate(value, format):
+	try:
+		return datetime.strptime(value, format).strftime("%Y-%m-%d")
+	except:
+		return None
+
+def castBoolean(value):
+  match value.upper():
+    case "T":
+      return True
+    case "TRUE":
+      return True
+    case "1":
+      return True
+    case "F":
+      return False
+    case "FALSE":
+      return False
+    case "0":
+      return False
+    case _:
+      return None
 		
-def batch_data(project_id, instance_id, database_id, table_id, batch_size, data_file, head_file, schema_file):
+def batch_data(project_id, instance_id, database_id, table_id, batch_size, data_file, head_file, schema_file, format_date):
 
     warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
 
     if project_id!="":
       os.environ["GCLOUD_PROJECT"] = project_id
 
+    if project_id=="" and os.environ["GCLOUD_PROJECT"]=="":
+        print("You need to fill the GCLOUD_PROJECT environment variable or the --project_id parameter with the name of the project in GCP.")
+        exit(1)
+
     spanner_client = spanner.Client()
     instance = spanner_client.instance(instance_id)
     database = instance.database(database_id)
     
-    schemafile = open(schema_file, 'r')
+    schemafile = open(schema_file, "r")
     schemareader = csv.reader(schemafile)
 
     collist = []
@@ -43,7 +79,7 @@ def batch_data(project_id, instance_id, database_id, table_id, batch_size, data_
     numcols = len(collist)
     
     datafile  = open(data_file, "r")
-    reader = csv.reader(datafile,delimiter=',')
+    reader = csv.reader(datafile,delimiter=",")
 
     datalist = []
     numberrow = 0
@@ -51,17 +87,27 @@ def batch_data(project_id, instance_id, database_id, table_id, batch_size, data_
     batchrows = int(batch_size)
 
     for row in reader:
-      if head_file=='true' and numberrow==0:
+      if head_file=="true" and numberrow==0:
         numberrow = numberrow + 1
         continue
 
       for columnValue in range(0,numcols):
-        if typelist[columnValue] == 'integer':
-          row[columnValue] = castInt(row[columnValue])
-        if typelist[columnValue] == 'float':
-          row[columnValue] = castFloat(row[columnValue])
-        if typelist[columnValue] == 'bytes':
-          row[columnValue] = base64.b64encode(row[columnValue])
+        match typelist[columnValue]:
+          case "integer":
+            row[columnValue] = castInt(row[columnValue])
+          case "float":
+            row[columnValue] = castFloat(row[columnValue])
+          case "bytes":
+            row[columnValue] = castBytes(row[columnValue])
+          case "date":
+            row[columnValue] = castDate(row[columnValue], format_date)
+          case "boolean":
+            row[columnValue] = castBoolean(row[columnValue])
+          case "string":
+            row[columnValue] = row[columnValue]
+          case _:
+            print("Data type {} is not supported".format(typelist[columnValue]))
+            exit(1)
       
       datalist.append(row)
 
@@ -77,34 +123,36 @@ def batch_data(project_id, instance_id, database_id, table_id, batch_size, data_
 
           datalist.clear() 
 
-          print ('Inserted {0} rows'.format(numberrow))
+          print ("Inserted {0} rows".format(numberrow))
 
     
-    print ('Finish! Inserted {0} rows'.format(numberrow))         
+    print ("Finish! Inserted {0} rows".format(numberrow))         
   		    		
     datafile.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument(
-        '--project_id', help='Your Google Project ID')
+        "--project_id", help="Your Google Project ID")
     parser.add_argument(
-        '--instance_id', help='Your Cloud Spanner instance ID')
+        "--instance_id", help="Your Cloud Spanner instance ID")
     parser.add_argument(
-        '--database_id', help='Your Cloud Spanner database ID')
+        "--database_id", help="Your Cloud Spanner database ID")
     parser.add_argument(
-    	  '--table_id', help='Your table name')
+    	  "--table_id", help="Your table name")
     parser.add_argument(
-		    '--batch_size', help='Number of rows to insert in a batch')
+		    "--batch_size", help="Number of rows to insert in a batch")
     parser.add_argument(
-		    '--data_file', help='Path of csv input data file')
+		    "--data_file", help="Path of csv input data file")
     parser.add_argument(
-		    '--head_file', help='If the header exists in the CSV, set the option to true to disregard it', default='true')
+		    "--head_file", help="If the header exists in the CSV, set the option to true to disregard it", default="true")
     parser.add_argument(
-        '--schema_file', help='Path of schema file describing the input data file')
+        "--schema_file", help="Path of schema file describing the input data file")
+    parser.add_argument(
+		    "--format_date", help="Sets the date format for conversion. Default is: %Y-%m-%d", default="%Y-%m-%d")
 		
     args = parser.parse_args()
 
-    batch_data(args.project_id, args.instance_id, args.database_id, args.table_id, args.batch_size, args.data_file, args.head_file, args.schema_file)
+    batch_data(args.project_id, args.instance_id, args.database_id, args.table_id, args.batch_size, args.data_file, args.head_file, args.schema_file, args.format_date)
